147b7ebbdd58bdd5db6aedc1781f171e6b63d3ae
Mattias Buelens <mattias@buelens.com>
2023-12-01 09:34:21 +0100

    Solve day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -27,9 +27,47 @@ fn part1(input: &Input) -> u32 {
         .sum()
 }
 
+fn part2_parse_digit(s: &str) -> Option<u32> {
+    if let Some(digit @ '1'..='9') = s.chars().next() {
+        Some(digit.to_digit(10).unwrap())
+    } else if s.starts_with("one") {
+        Some(1)
+    } else if s.starts_with("two") {
+        Some(2)
+    } else if s.starts_with("three") {
+        Some(3)
+    } else if s.starts_with("four") {
+        Some(4)
+    } else if s.starts_with("five") {
+        Some(5)
+    } else if s.starts_with("six") {
+        Some(6)
+    } else if s.starts_with("seven") {
+        Some(7)
+    } else if s.starts_with("eight") {
+        Some(8)
+    } else if s.starts_with("nine") {
+        Some(9)
+    } else {
+        None
+    }
+}
+
 #[aoc(day1, part2)]
-fn part2(input: &Input) -> String {
-    todo!()
+fn part2(input: &Input) -> u32 {
+    input
+        .iter()
+        .map(|line| {
+            let first_digit = (0..line.len())
+                .find_map(|i| part2_parse_digit(&line[i..]))
+                .unwrap();
+            let last_digit = (0..line.len())
+                .rev()
+                .find_map(|i| part2_parse_digit(&line[i..]))
+                .unwrap();
+            first_digit * 10 + last_digit
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -47,6 +85,13 @@ treb7uchet";
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        let example = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!(part2(&parse(example)), 281);
     }
 }
