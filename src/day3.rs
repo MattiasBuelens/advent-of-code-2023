b52055022e874b2dbbaf5280fbdebed4443017b8
Mattias Buelens <mattias@buelens.com>
2023-12-03 19:21:45 +0100

    Solve day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -7,16 +7,21 @@ use crate::util::Vector2D;
 
 #[derive(Debug)]
 struct Schematic {
-    numbers: HashMap<Vector2D, String>,
-    symbols: HashMap<Vector2D, char>,
+    things: HashMap<Vector2D, EngineThing>,
+}
+
+#[derive(Debug)]
+struct EngineThing {
+    symbol: char,
+    part_numbers: Vec<u32>,
 }
 
 impl FromStr for Schematic {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut numbers = HashMap::new();
-        let mut symbols = HashMap::new();
+        let mut numbers = Vec::new();
+        let mut things = HashMap::new();
         for (y, line) in s.lines().enumerate() {
             let mut current_number: Option<(Vector2D, String)> = None;
             for (x, c) in line.chars().enumerate() {
@@ -26,18 +31,52 @@ impl FromStr for Schematic {
                     number.push(c);
                 } else {
                     if c != '.' {
-                        symbols.insert(pos, c);
+                        let thing = EngineThing {
+                            symbol: c,
+                            part_numbers: vec![],
+                        };
+                        things.insert(pos, thing);
                     }
                     if let Some((pos, number)) = current_number.take() {
-                        numbers.insert(pos, number);
+                        numbers.push((pos, number));
                     }
                 }
             }
             if let Some((pos, number)) = current_number.take() {
-                numbers.insert(pos, number);
+                numbers.push((pos, number));
             }
         }
-        Ok(Self { numbers, symbols })
+        let mut result = Self { things };
+        for (pos, number) in numbers {
+            result.add_part_number(pos, &number);
+        }
+        Ok(result)
+    }
+}
+
+impl Schematic {
+    fn adjacent_symbol_pos(&mut self, pos: Vector2D, width: i32) -> Option<Vector2D> {
+        for y in pos.y() - 1..=pos.y() + 1 {
+            let min_x = pos.x() - 1;
+            let max_x = pos.x() + width;
+            for x in min_x..=max_x {
+                let pos = Vector2D::new(x, y);
+                if self.things.contains_key(&pos) {
+                    return Some(pos);
+                }
+            }
+        }
+        None
+    }
+
+    fn add_part_number(&mut self, pos: Vector2D, number: &str) {
+        if let Some(thing_pos) = self.adjacent_symbol_pos(pos, number.len() as i32) {
+            self.things
+                .get_mut(&thing_pos)
+                .unwrap()
+                .part_numbers
+                .push(number.parse().unwrap())
+        }
     }
 }
 
@@ -46,41 +85,30 @@ fn parse(input: &str) -> Schematic {
     input.parse().unwrap()
 }
 
-impl Schematic {
-    fn part_numbers(&self) -> Vec<u32> {
-        self.numbers
-            .iter()
-            .filter(|(pos, number)| {
-                let width = number.len() as i32;
-                
-                (pos.y() - 1..=pos.y() + 1).any(move |y| {
-                    let min_x = pos.x() - 1;
-                    let max_x = pos.x() + width;
-                    (min_x..=max_x).any(move |x| self.symbols.contains_key(&Vector2D::new(x, y)))
-                })
-            })
-            .map(|(_pos, number)| number.parse().unwrap())
-            .collect()
-    }
-}
-
 #[aoc(day3, part1)]
 fn part1(input: &Schematic) -> u32 {
-    input.part_numbers().into_iter().sum()
+    input
+        .things
+        .values()
+        .flat_map(|thing| thing.part_numbers.iter())
+        .sum()
 }
 
 #[aoc(day3, part2)]
-fn part2(_input: &Schematic) -> String {
-    todo!()
+fn part2(input: &Schematic) -> u32 {
+    input
+        .things
+        .values()
+        .filter(|thing| thing.symbol == '*' && thing.part_numbers.len() == 2)
+        .map(|thing| thing.part_numbers[0] * thing.part_numbers[1])
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn part1_example() {
-        let input = "467..114..
+    const INPUT: &str = "467..114..
 ...*......
 ..35..633.
 ......#...
@@ -90,11 +118,14 @@ mod tests {
 ......755.
 ...$.*....
 .664.598..";
-        assert_eq!(part1(&parse(input)), 4361);
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse(INPUT)), 4361);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse(INPUT)), 467835);
     }
 }
