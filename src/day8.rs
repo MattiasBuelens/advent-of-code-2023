706c75bab8c0e681aaf2dfdcae3af642d09a1ee7
Mattias Buelens <mattias@buelens.com>
2023-12-08 14:25:10 +0100

    Solve day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,7 +1,8 @@
-use std::collections::{HashMap, HashSet};
-use std::hash::{Hash, Hasher};
+use std::collections::HashMap;
 
 use aoc_runner_derive::{aoc, aoc_generator};
+
+use crate::util::lcm_64;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Direction {
@@ -46,12 +47,11 @@ fn parse(input: &str) -> Input {
     Input { steps, nodes }
 }
 
-#[aoc(day8, part1)]
-fn part1(input: &Input) -> u32 {
-    let mut current = &"AAA".to_string();
+fn solve(start: &String, is_goal: impl Fn(&String) -> bool, input: &Input) -> i64 {
+    let mut current = start;
     let mut steps = input.steps.iter().cycle();
     let mut step_count = 0;
-    while current != "ZZZ" {
+    while !is_goal(current) {
         let current_node = input.nodes.get(current).unwrap();
         let step = steps.next().unwrap();
         current = match step {
@@ -63,79 +63,25 @@ fn part1(input: &Input) -> u32 {
     step_count
 }
 
-#[derive(Debug, Clone)]
-struct State<'a> {
-    label: &'a String,
-    step: usize,
-    total_steps: usize,
-}
-
-impl<'a> PartialEq<Self> for State<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.label.eq(other.label) && self.step.eq(&other.step)
-    }
-}
-
-impl<'a> Eq for State<'a> {}
-
-impl<'a> Hash for State<'a> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.label.hash(state);
-        self.step.hash(state);
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Cycle {
-    start: usize,
-    length: usize,
-    steps_from_start_to_destination: usize,
-}
-
-fn find_cycle(start: &String, input: &Input) -> Cycle {
-    let mut steps = input.steps.iter().cycle();
-    let mut states = HashSet::new();
-    let mut state = State {
-        label: start,
-        step: 0,
-        total_steps: 0,
-    };
-    let mut steps_to_destination = None;
-    while !states.contains(&state) {
-        states.insert(state.clone());
-        let current_node = input.nodes.get(state.label).unwrap();
-        let step = steps.next().unwrap();
-        state.label = match step {
-            Direction::Left => &current_node.left,
-            Direction::Right => &current_node.right,
-        };
-        state.total_steps += 1;
-        state.step = state.total_steps % input.steps.len();
-        if state.label.ends_with('Z') && steps_to_destination.is_none() {
-            steps_to_destination = Some(state.total_steps);
-        }
-    }
-    let first_state = states.get(&state).unwrap();
-    Cycle {
-        start: first_state.total_steps,
-        length: state.total_steps - first_state.total_steps,
-        steps_from_start_to_destination: steps_to_destination.unwrap() - first_state.total_steps,
-    }
+#[aoc(day8, part1)]
+fn part1(input: &Input) -> i64 {
+    solve(&"AAA".to_string(), |label| label == "ZZZ", input)
 }
 
 #[aoc(day8, part2)]
-fn part2(input: &Input) -> u32 {
+fn part2(input: &Input) -> i64 {
     let start_labels = input
         .nodes
         .keys()
         .filter(|label| label.ends_with('A'))
         .collect::<Vec<_>>();
-    let cycles = start_labels
+    let solutions = start_labels
         .iter()
-        .map(|start| (start, find_cycle(start, input)))
+        .map(|start| solve(start, |label| label.ends_with('Z'), input))
         .collect::<Vec<_>>();
-    dbg!(&cycles);
-    todo!()
+    solutions
+        .iter()
+        .fold(1, |steps, &solution| lcm_64(steps, solution))
 }
 
 #[cfg(test)]
