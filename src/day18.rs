7391e82924c911641c582f19d44eb24b7ec3523a
Mattias Buelens <mattias@buelens.com>
2023-12-19 20:12:56 +0100

    Revert "Rework day 18 part 1"
    This reverts commit cd7ce25c5b627eb33b710523c27bd98d0503b4da.

--- src/day18.rs
+++ src/day18.rs
@@ -1,5 +1,4 @@
-use std::cmp::Ordering;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::str::FromStr;
 
 use aoc_runner_derive::{aoc, aoc_generator};
@@ -42,206 +41,17 @@ fn parse(input: &str) -> Vec<Instruction> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
-struct Interval {
-    from: i32,
-    to: i32,
-}
-
-impl Interval {
-    fn new(from: i32, to: i32) -> Self {
-        debug_assert!(from <= to);
-        Self { from, to }
-    }
-
-    fn contains(&self, value: i32) -> bool {
-        self.from <= value && value <= self.to
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-struct Rectangle {
-    left: i32,
-    top: i32,
-    width: i32,
-    height: i32,
-}
-
-impl Rectangle {
-    fn from_points(from: Vector2D, to: Vector2D) -> Self {
-        debug_assert!(from.x() <= to.x());
-        debug_assert!(from.y() <= to.y());
-        Self {
-            left: from.x(),
-            top: from.y(),
-            width: to.x() - from.x() + 1,
-            height: to.y() - from.y() + 1,
-        }
-    }
-
-    fn right(&self) -> i32 {
-        self.left + self.width
-    }
-
-    fn bottom(&self) -> i32 {
-        self.top + self.height
-    }
-
-    fn expand(self, other: Self) -> Self {
-        let left = self.left.min(other.left);
-        let top = self.top.min(other.top);
-        let width = self.right().max(other.right()) - left;
-        let height = self.bottom().max(other.bottom()) - top;
-        Self {
-            left,
-            top,
-            width,
-            height,
-        }
-    }
-}
-
-#[derive(Debug, Default, Clone)]
-struct IntervalSet {
-    list: Vec<Interval>,
-}
-
-impl IntervalSet {
-    fn new() -> Self {
-        Self { list: Vec::new() }
-    }
-
-    fn insert(&mut self, interval: Interval) {
-        let index = self
-            .list
-            .binary_search(&interval)
-            .expect_err("interval already exists");
-        self.list.insert(index, interval);
-    }
-
-    fn find(&self, value: i32) -> Option<&Interval> {
-        self.list
-            .binary_search_by(|interval| {
-                if interval.to < value {
-                    Ordering::Less
-                } else if interval.from > value {
-                    Ordering::Greater
-                } else {
-                    Ordering::Equal
-                }
-            })
-            .ok()
-            .map(|index| &self.list[index])
-    }
-
-    fn bounds(&self) -> Option<Interval> {
-        if self.list.is_empty() {
-            return None;
-        }
-        return Some(Interval::new(
-            self.list.first().unwrap().from,
-            self.list.last().unwrap().to,
-        ));
-    }
-}
-
-#[derive(Debug, Clone)]
-struct LineMap {
-    horizontal: HashMap<i32, IntervalSet>,
-    vertical: HashMap<i32, IntervalSet>,
-}
-
-impl LineMap {
-    fn new() -> Self {
-        Self {
-            horizontal: HashMap::new(),
-            vertical: HashMap::new(),
-        }
-    }
-
-    fn insert(&mut self, left: Vector2D, right: Vector2D) {
-        let (left, right) = (left.min(right), left.max(right));
-        if left.y() == right.y() {
-            self.horizontal
-                .entry(left.y())
-                .or_default()
-                .insert(Interval::new(left.x(), right.x()));
-        } else if left.x() == right.x() {
-            self.vertical
-                .entry(left.x())
-                .or_default()
-                .insert(Interval::new(left.y(), right.y()));
-        } else {
-            panic!("not a line: left = {left:?}, right = {right:?}");
-        }
-    }
-
-    fn find(&self, pos: &Vector2D) -> Option<(Vector2D, Vector2D)> {
-        self.horizontal
-            .get(&pos.y())
-            .and_then(|set| set.find(pos.x()))
-            .map(|interval| {
-                (
-                    Vector2D::new(interval.from, pos.y()),
-                    Vector2D::new(interval.to, pos.y()),
-                )
-            })
-            .or_else(|| {
-                self.vertical
-                    .get(&pos.x())
-                    .and_then(|set| set.find(pos.y()))
-                    .map(|interval| {
-                        (
-                            Vector2D::new(pos.x(), interval.from),
-                            Vector2D::new(pos.x(), interval.to),
-                        )
-                    })
-            })
-    }
-
-    fn contains(&self, pos: &Vector2D) -> bool {
-        self.find(pos).is_some()
-    }
-
-    fn bounds(&self) -> Option<Rectangle> {
-        let mut bounds: Option<Rectangle> = None;
-        for (&y, set) in &self.horizontal {
-            if let Some(set_interval) = set.bounds() {
-                let set_bounds = Rectangle::from_points(
-                    Vector2D::new(set_interval.from, y),
-                    Vector2D::new(set_interval.to, y),
-                );
-                bounds = Some(match bounds {
-                    Some(bounds) => bounds.expand(set_bounds),
-                    None => set_bounds,
-                });
-            }
-        }
-        for (&x, set) in &self.vertical {
-            if let Some(set_interval) = set.bounds() {
-                let set_bounds = Rectangle::from_points(
-                    Vector2D::new(x, set_interval.from),
-                    Vector2D::new(x, set_interval.to),
-                );
-                bounds = Some(match bounds {
-                    Some(bounds) => bounds.expand(set_bounds),
-                    None => set_bounds,
-                });
-            }
-        }
-        bounds
-    }
-}
-
 #[aoc(day18, part1)]
 fn part1(plan: &[Instruction]) -> usize {
-    let mut edge = LineMap::new();
+    let mut edge = HashSet::<Vector2D>::new();
     // Dig out the edge
     let mut pos = Vector2D::new(0, 0);
+    edge.insert(pos);
     for instruction in plan {
-        let new_pos = pos + (instruction.dir.step() * instruction.meters);
-        edge.insert(pos, new_pos);
-        pos = new_pos;
+        for _ in 0..instruction.meters {
+            pos += instruction.dir.step();
+            edge.insert(pos);
+        }
     }
     // Dig out the interior
     let trench = fill_trench(&edge);
@@ -255,24 +65,22 @@ enum FollowingState {
     FollowingDown,
 }
 
-fn fill_trench(edge: &LineMap) -> HashSet<Vector2D> {
-    let bounds = edge.bounds().unwrap();
-    let min_x = bounds.left;
-    let max_x = bounds.right();
-    let min_y = bounds.top;
-    let max_y = bounds.bottom();
-    let mut trench = HashSet::new();
+fn fill_trench(edge: &HashSet<Vector2D>) -> HashSet<Vector2D> {
+    let min_x = edge.iter().map(|pos| pos.x()).min().unwrap();
+    let max_x = edge.iter().map(|pos| pos.x()).max().unwrap();
+    let min_y = edge.iter().map(|pos| pos.y()).min().unwrap();
+    let max_y = edge.iter().map(|pos| pos.y()).max().unwrap();
+    let mut trench = edge.clone();
     for y in min_y..=max_y {
         let mut following = FollowingState::NotFollowing;
         let mut inside = false;
         for x in min_x..=max_x {
             let pos = Vector2D::new(x, y);
-            let on_edge = edge.contains(&pos);
             let pos_up = pos + Vector2D::new(0, -1);
             let pos_down = pos + Vector2D::new(0, 1);
             if following == FollowingState::FollowingUp {
                 // Following an edge with a turn at the top
-                if !on_edge {
+                if !edge.contains(&pos) {
                     // Stopped following the edge
                     let prev_pos_up = pos + Vector2D::new(-1, -1);
                     if edge.contains(&prev_pos_up) {
@@ -285,7 +93,7 @@ fn fill_trench(edge: &LineMap) -> HashSet<Vector2D> {
                 }
             } else if following == FollowingState::FollowingDown {
                 // Following an edge with a turn at the bottom
-                if !on_edge {
+                if !edge.contains(&pos) {
                     // Stopped following the edge
                     let prev_pos_down = pos + Vector2D::new(-1, 1);
                     if edge.contains(&prev_pos_down) {
@@ -296,7 +104,7 @@ fn fill_trench(edge: &LineMap) -> HashSet<Vector2D> {
                     }
                     following = FollowingState::NotFollowing;
                 }
-            } else if on_edge {
+            } else if edge.contains(&pos) {
                 match (edge.contains(&pos_up), edge.contains(&pos_down)) {
                     (true, true) => {
                         // Crossed the edge
@@ -313,7 +121,7 @@ fn fill_trench(edge: &LineMap) -> HashSet<Vector2D> {
                     (false, false) => panic!("invalid crossing at {pos:?}"),
                 };
             }
-            if inside || on_edge {
+            if inside {
                 trench.insert(pos);
             }
         }
