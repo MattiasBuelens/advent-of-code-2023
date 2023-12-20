d64cc801565ae2911552e5d719e9e4b029cd949c
Mattias Buelens <mattias@buelens.com>
2023-12-20 17:50:51 +0100

    Solve day 20 part 1

--- src/day20.rs
+++ src/day20.rs
@@ -1,9 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Debug, Clone)]
 struct Module {
+    name: String,
     kind: ModuleKind,
     inputs: Vec<String>,
     outputs: Vec<String>,
@@ -36,6 +37,7 @@ fn parse(input: &str) -> ModuleGraph {
         graph.insert(
             name.to_string(),
             Module {
+                name: name.to_string(),
                 kind,
                 inputs: vec![],
                 outputs,
@@ -59,10 +61,94 @@ fn parse(input: &str) -> ModuleGraph {
     graph
 }
 
+impl Module {
+    fn receive(&mut self, pulse: bool, sender: &str) -> Option<bool> {
+        match &mut self.kind {
+            ModuleKind::FlipFlop(state) => {
+                // If a flip-flop module receives a low pulse, it flips between on and off.
+                // If it was off, it turns on and sends a high pulse.
+                // If it was on, it turns off and sends a low pulse.
+                if !pulse {
+                    *state = !*state;
+                    Some(*state)
+                } else {
+                    None
+                }
+            }
+            ModuleKind::Conjunction(state) => {
+                // When a pulse is received, the conjunction module first updates its memory for that input.
+                let sender_idx = self
+                    .inputs
+                    .iter()
+                    .position(|input| input == sender)
+                    .unwrap();
+                state[sender_idx] = pulse;
+                // Then, if it remembers high pulses for all inputs, it sends a low pulse;
+                // otherwise, it sends a high pulse.
+                if state.iter().all(|&x| x) {
+                    Some(false)
+                } else {
+                    Some(true)
+                }
+            }
+            ModuleKind::Broadcast => {
+                // When it receives a pulse, it sends the same pulse to all of its destination modules.
+                Some(pulse)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Pulse {
+    value: bool,
+    sender: String,
+    receiver: String,
+}
+
+#[derive(Debug, Clone, Default)]
+struct PulseCounts {
+    low: u64,
+    high: u64,
+}
+
+fn push_button(graph: &mut ModuleGraph, counts: &mut PulseCounts) {
+    let mut queue = VecDeque::<Pulse>::new();
+    // When you push the button, a single low pulse is sent directly to the broadcaster module.
+    queue.push_back(Pulse {
+        value: false,
+        sender: "button".to_string(),
+        receiver: "broadcaster".to_string(),
+    });
+    counts.low += 1;
+    while let Some(pulse) = queue.pop_front() {
+        if let Some(receiver) = graph.get_mut(&pulse.receiver) {
+            if let Some(new_pulse) = receiver.receive(pulse.value, &pulse.sender) {
+                for output in &receiver.outputs {
+                    queue.push_back(Pulse {
+                        value: new_pulse,
+                        sender: receiver.name.clone(),
+                        receiver: output.clone(),
+                    });
+                    if new_pulse {
+                        counts.high += 1;
+                    } else {
+                        counts.low += 1;
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[aoc(day20, part1)]
 fn part1(graph: &ModuleGraph) -> u64 {
-    dbg!(&graph);
-    todo!()
+    let mut graph = graph.clone();
+    let mut counts = PulseCounts::default();
+    for _ in 0..1000 {
+        push_button(&mut graph, &mut counts);
+    }
+    counts.low * counts.high
 }
 
 #[aoc(day20, part2)]
