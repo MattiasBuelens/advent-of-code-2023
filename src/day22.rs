a40d1f1d6f07401b024d175d9501a4c76557964b
Mattias Buelens <mattias@buelens.com>
2023-12-22 10:55:55 +0100

    Rename

--- src/day22.rs
+++ src/day22.rs
@@ -77,33 +77,33 @@ fn drop_bricks(bricks: &[Brick]) -> (Vec<Brick>, HashSet<Vector3D>) {
     // Drop bricks, from lowest to highest
     let mut sorted_bricks = bricks.to_vec();
     sorted_bricks.sort_by_key(|brick| brick.start.z());
-    let mut dropped_bricks = vec![];
-    let mut dropped_cubes = HashSet::<Vector3D>::new();
+    let mut bricks = vec![];
+    let mut cubes = HashSet::<Vector3D>::new();
     for mut brick in sorted_bricks {
-        while let Some(next_brick) = try_drop_brick(&brick, &dropped_cubes) {
+        while let Some(next_brick) = try_drop_brick(&brick, &cubes) {
             brick = next_brick;
         }
-        dropped_bricks.push(brick);
-        dropped_cubes.extend(brick.cubes());
+        bricks.push(brick);
+        cubes.extend(brick.cubes());
     }
-    dropped_bricks.sort_by_key(|brick| brick.start.z());
-    (dropped_bricks, dropped_cubes)
+    bricks.sort_by_key(|brick| brick.start.z());
+    (bricks, cubes)
 }
 
 #[aoc(day22, part1)]
 fn part1(input: &[Brick]) -> usize {
-    let (dropped_bricks, dropped_cubes) = drop_bricks(input);
+    let (bricks, cubes) = drop_bricks(input);
     // Check which bricks can be disintegrated
     let mut num_disintegrate = 0;
-    for brick in &dropped_bricks {
+    for brick_to_disintegrate in &bricks {
         let mut can_disintegrate = true;
-        for other_brick in &dropped_bricks {
-            if other_brick == brick || !other_brick.can_drop() {
+        for other_brick in &bricks {
+            if other_brick == brick_to_disintegrate || !other_brick.can_drop() {
                 continue;
             }
             // Disintegrate the brick
-            let mut new_cubes = dropped_cubes.clone();
-            for cube in brick.cubes() {
+            let mut new_cubes = cubes.clone();
+            for cube in brick_to_disintegrate.cubes() {
                 new_cubes.remove(&cube);
             }
             // Remove the other brick too, so it doesn't collide with itself
@@ -125,13 +125,13 @@ fn part1(input: &[Brick]) -> usize {
 
 #[aoc(day22, part2)]
 fn part2(input: &[Brick]) -> usize {
-    let (dropped_bricks, _) = drop_bricks(input);
+    let (bricks, _) = drop_bricks(input);
     // Check how many bricks would fall if we were to disintegrate every brick (separately)
     let mut total_dropped = 0;
-    for &brick_to_disintegrate in &dropped_bricks {
+    for &brick_to_disintegrate in &bricks {
         // Let all other bricks drop again
         let mut new_cubes = HashSet::new();
-        for &other_brick in &dropped_bricks {
+        for &other_brick in &bricks {
             if brick_to_disintegrate == other_brick {
                 continue;
             }
