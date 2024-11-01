83629ab3e2384c66637088c798d5bf066acdf8d2
Mattias Buelens <mattias@buelens.com>
2024-11-02 00:47:49 +0100

    Optimize by replacing strings with numeric IDs

--- src/day25.rs
+++ src/day25.rs
@@ -4,34 +4,44 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use rand::seq::IteratorRandom;
 use rand::SeedableRng;
 
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Copy, Clone)]
+struct NodeId(u64);
+
 #[derive(Debug, Default, Clone)]
 struct Graph {
-    connections: HashMap<String, Vec<String>>,
-    weights: HashMap<String, usize>,
+    connections: HashMap<NodeId, Vec<NodeId>>,
+    weights: HashMap<NodeId, usize>,
 }
 
 impl Graph {
-    fn add(&mut self, from: &str, to: &str) {
-        self.connections
-            .entry(from.to_string())
-            .or_default()
-            .push(to.to_string());
-        self.connections
-            .entry(to.to_string())
-            .or_default()
-            .push(from.to_string());
-        self.weights.entry(from.to_string()).or_insert(1);
-        self.weights.entry(to.to_string()).or_insert(1);
+    fn add(&mut self, from: NodeId, to: NodeId) {
+        self.connections.entry(from).or_default().push(to);
+        self.connections.entry(to).or_default().push(from);
+        self.weights.entry(from).or_insert(1);
+        self.weights.entry(to).or_insert(1);
     }
 }
 
 #[aoc_generator(day25)]
 fn parse(input: &str) -> Graph {
     let mut graph = Graph::default();
+    let mut get_id = {
+        let mut ids = HashMap::<String, NodeId>::new();
+        let mut next_id = 0u64;
+        move |name: &str| -> NodeId {
+            *ids.entry(name.to_string()).or_insert_with(|| {
+                let id = next_id;
+                next_id += 1;
+                NodeId(id)
+            })
+        }
+    };
+
     for line in input.lines() {
         let (from, connections) = line.split_once(": ").unwrap();
+        let from_id = get_id(from);
         for to in connections.split(' ') {
-            graph.add(from, to);
+            graph.add(from_id, get_id(to));
         }
     }
     graph
@@ -44,16 +54,16 @@ fn part1(graph: &Graph) -> usize {
 }
 
 impl Graph {
-    fn edges(&self) -> impl Iterator<Item = (&str, &str)> {
+    fn edges(&self) -> impl Iterator<Item = (NodeId, NodeId)> + '_ {
         self.connections.iter().flat_map(|(from, connections)| {
             connections
                 .iter()
                 .filter(move |to| {
                     // Each edge is stored twice in the map, but we only want to return them once
-                    // Break symmetry by requiring alphabetical order
+                    // Break symmetry by requiring numerical order
                     from < to
                 })
-                .map(move |to| (from.as_ref(), to.as_ref()))
+                .map(move |to| (*from, *to))
                 .collect::<Vec<_>>() // for size_hint()
         })
     }
@@ -61,21 +71,21 @@ impl Graph {
     /// Remove the edge `(from, to)` from the graph, and replace it with a new node
     /// that is connected with all nodes connected to `from` or `to` in the original graph.
     /// https://en.wikipedia.org/wiki/Karger%27s_algorithm#Contraction_algorithm
-    fn contract(&mut self, from: &str, to: &str) {
+    fn contract(&mut self, from: NodeId, to: NodeId) {
         // Merge `from` connections with `to` connections.
-        let to_connections = self.connections.remove(to).unwrap();
-        let from_connections = self.connections.get_mut(from).unwrap();
+        let to_connections = self.connections.remove(&to).unwrap();
+        let from_connections = self.connections.get_mut(&from).unwrap();
         from_connections.extend(to_connections);
-        from_connections.retain(|x| x != from && x != to); // no self-loops
+        from_connections.retain(|x| x != &from && x != &to); // no self-loops
 
         // Merge weights.
-        *self.weights.get_mut(from).unwrap() += self.weights.remove(to).unwrap();
+        *self.weights.get_mut(&from).unwrap() += self.weights.remove(&to).unwrap();
 
         // Replace `to` with `from` in the rest of the graph.
         for (_, connections) in self.connections.iter_mut() {
             for other in connections.iter_mut() {
-                if other == to {
-                    *other = from.to_string();
+                if *other == to {
+                    *other = from;
                 }
             }
         }
@@ -92,8 +102,7 @@ fn split_graph(input: &Graph) -> (usize, usize) {
         let mut graph = input.clone();
         while graph.connections.len() > 2 {
             let (from, to) = graph.edges().choose(&mut rng).unwrap();
-            let (from, to) = (from.to_string(), to.to_string());
-            graph.contract(&from, &to);
+            graph.contract(from, to);
         }
         // Only two nodes left in the contracted graph
         // Check if they're separated by exactly 3 edges
